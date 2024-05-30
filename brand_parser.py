d02dd4a94a78de08d533dd847b8da71841509d98
tasleem-khan-00 <tahseemuploaddrive@gmail.com>
2024-05-30 10:19:17 +0530

    Added Burberry Parser

--- brand_parser.py
+++ brand_parser.py
@@ -1,4 +1,7 @@
 from main_parser import WebsiteParser
+from urllib.parse import urljoin
+import re
+
 class BrandNameParser(WebsiteParser):
 
     def __init__(self, directory):
@@ -44,3 +47,61 @@ class BrandNameParser(WebsiteParser):
             parsed_data.append(product_data)
         #Must return list of lists containing product data to properly write to csv.
         return parsed_data
+
+
+class BurberryParser(WebsiteParser):
+
+    def __init__(self, directory):
+        self.brand = "burberry"
+        self.directory = directory
+        
+    def parse_product_blocks(self, soup, category):
+    
+        product_blocks = soup.select('a.product-card-v2-anchor, a.redesigned-product-card__link')
+        print(len(product_blocks))
+        parsed_data = []
+        
+        column_names = [
+            'category', 'name',  'product_id', 'product_url', 'image_url','full_price','discount_price','tag(s)']
+            
+        parsed_data.append(column_names)
+        
+        base_url = soup.find('meta',{'property':'org:url'})
+        if base_url:
+            base_url = base_url['content']
+        else:
+           base_url = "https://us.burberry.com/"
+         
+        for product in product_blocks:  
+        
+            name = product.select_one('.product-card-v2-title, .product-card-content__title').get_text(strip=True)
+            
+            product_link = urljoin(base_url,product['href'])
+            
+            product_id = re.search('\-p(\d+)/?$',product_link,flags=re.I)
+            product_id = product_id.group(1) if product_id else "unavailable"
+                
+            price = product.select_one('.product-card-price__current-price, .product-card-v2-price__current').get_text(strip=True)
+            
+            discount_price = "" #empty for now and can be added later if found
+            
+            image_url = product.select_one('.redesigned-product-card__picture img, .product-card-v2-carousel-container__media__picture img')['src']
+            
+            tags = product.select_one('.product-card-labels__flag, .product-card-v2-carousel-labels__label')
+            tags = tags.get_text(strip=True) if tags else ""
+                  
+            product_data = [
+                category,
+                name,
+                product_id,
+                product_link,
+                image_url,
+                price,
+                discount_price,
+                tags
+            ]
+            
+            parsed_data.append(product_data)
+            
+        return parsed_data          
+        
--- run_parser.py
+++ run_parser.py
@@ -1,9 +1,20 @@
-from brand_parser import BrandNameParser
+from brand_parser import BrandNameParser, BurberryParser
 
 
 ###START PARSER##
 output_directory_path = 'parser-output'
+
+
 directory_path = 'brand_name'
 
 brand_name_parser = BrandNameParser(output_directory_path)
-brand_name_parser.parse_directory(directory_path)
+#brand_name_parser.parse_directory(directory_path)
+
+
+# BurberryParser RUN
+directory_path = 'burberry'
+
+burberry_parser = BurberryParser(output_directory_path)
+burberry_parser.parse_directory(directory_path)
+
+
